02264f132c8ec0d17f7a6b4723824b29551ace68
201dreamers <201dreamers@gmail.com>
2023-12-14 12:36:54 +0200

    change mapping for comments & add plugin for multiple cursors

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -19,6 +19,7 @@ return {
     { "mbbill/undotree" },
     { "ThePrimeagen/harpoon" },
     { "nvim-pack/nvim-spectre" },
+    { "mg979/vim-visual-multi" },
 
 
     -- ---------
@@ -46,7 +47,21 @@ return {
     -- -----------------------------
     {
         "numToStr/Comment.nvim",
-        config = {},
+        config = {
+            toggler = {
+                ---Line-comment toggle keymap
+                line = '<leader>/',
+                ---Block-comment toggle keymap
+                block = 'gbc',
+            },
+            ---LHS of operator-pending mappings in NORMAL and VISUAL mode
+            opleader = {
+                ---Line-comment keymap
+                line = '<leader>/',
+                ---Block-comment keymap
+                block = 'gb',
+            },
+        },
         lazy = false,
     },
     {
