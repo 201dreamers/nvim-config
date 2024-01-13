c645dc2f7fd07284db49b08184b7920616b0749e
201dreamers <201dreamers@gmail.com>
2024-01-13 17:21:19 +0200

    fix debubpy path

--- after/plugin/languages/debuggers/python.lua
+++ after/plugin/languages/debuggers/python.lua
@@ -1,10 +1,9 @@
-local mason_ok, mason_registry = pcall(require, "mason-registry")
-local dap_python_ok, dap_python = pcall(require, "dap-python")
-if not (mason_ok and dap_python_ok) then
+local ok, dap_python = pcall(require, "dap-python")
+if not ok then
 	return
 end
 
-local debugpy = mason_registry.get_package("debugpy") -- note that this will error if you provide a non-existent package name
-local debugpy_python_path = debugpy:get_install_path() .. "/venv/bin/python"
+local debugpy_python_path = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python"
 
+dap_python.test_runner = "pytest"
 dap_python.setup(debugpy_python_path)
