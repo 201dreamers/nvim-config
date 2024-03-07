7c74da87b2dfd7e5bc08ab21ae04d844fd421f28
201dreamers <201dreamers@gmail.com>
2024-03-07 14:36:04 +0200

    set telescope find text to ss instead of st

--- after/plugin/spectre.lua
+++ after/plugin/spectre.lua
@@ -8,11 +8,14 @@ end
 -- --------
 local map = vim.keymap.set
 
-map("n", "<leader>ss", spectre.toggle, { desc = "toggle spectre" })
-map("n", "<leader>sw", function() spectre.open_visual({ select_word = true }) end, { desc = "spectre current word" })
+map("n", "<leader>st", spectre.toggle, { desc = "toggle spectre" })
+map("n", "<leader>sw", function()
+    spectre.open_visual({ select_word = true })
+end, { desc = "spectre current word" })
 map("v", "<leader>sw", function()
     vim.cmd.stopinsert()
     spectre.open_visual()
 end, { desc = "spectre current word" })
-map("n", "<leader>sp", function() spectre.open_file_search({ select_word = true }) end,
-    { desc = "spectre on current file" })
+map("n", "<leader>sp", function()
+    spectre.open_file_search({ select_word = true })
+end, { desc = "spectre on current file" })
