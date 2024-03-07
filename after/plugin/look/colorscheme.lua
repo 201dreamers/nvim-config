1fda5fd7e736f9183b7362bdd335cc58d140a0fe
201dreamers <201dreamers@gmail.com>
2024-03-07 12:20:31 +0200

    update colorscheme file

--- after/plugin/look/colorscheme.lua
+++ after/plugin/look/colorscheme.lua
@@ -10,43 +10,44 @@
 -- ------
 -- General
 -- ------
-local function set_colorscheme(theme_name, plugin_name, opts)
-    local ok, plugin = pcall(require, plugin_name)
+local function set_colorscheme(plugin_name, theme_name, opts)
+	local ok, plugin = pcall(require, plugin_name)
 
-    if not ok then
-        return
-    end
+	if not ok then
+		return
+	end
 
-    plugin.setup(opts)
-    vim.cmd.colorscheme(theme_name)
+	plugin.setup(opts)
+	vim.cmd.colorscheme(theme_name)
 end
 
 -- ---------------------
 -- Themes configurations
 -- ---------------------
 local catpuccin_opts = {
-    flavour = "mocha",              -- latte, frappe, macchiato, mocha
-    transparent_background = true,  -- disables setting the background color.
-    show_end_of_buffer = true,      -- shows the '~' characters after the end of buffers
-    no_bold = true,                 -- Force no bold
+	flavour = "mocha", -- latte, frappe, macchiato, mocha
+	transparent_background = true, -- disables setting the background color.
+	show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+	no_bold = true, -- Force no bold
 }
 
 local kanagawa_opts = {
-    compile = false,             -- enable compiling the colorscheme
-    undercurl = true,            -- enable undercurls
-    commentStyle = { italic = true },
-    functionStyle = {},
-    keywordStyle = { italic = true},
-    statementStyle = { bold = true },
-    typeStyle = {},
-    transparent = true,          -- do not set background color
-    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-    theme = "dragon",            -- Load "wave" theme when 'background' option is not set
+	compile = false, -- enable compiling the colorscheme
+	undercurl = true, -- enable undercurls
+	commentStyle = { italic = true },
+	functionStyle = {},
+	keywordStyle = { italic = true },
+	statementStyle = { bold = false },
+	typeStyle = {},
+	transparent = true, -- do not set background color
+	dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+	terminalColors = true, -- define vim.g.terminal_color_{0,17}
+	theme = "dragon", -- Load "wave" theme when 'background' option is not set
 }
+
 
 -- ---------------
 -- Set colorscheme
 -- ---------------
 -- set_colorscheme("catpuccin", "catpuccin", catpuccin_opts)
-set_colorscheme("kanagawa", "kanagawa", kanagawa_opts)
+set_colorscheme("kanagawa", "kanagawa-dragon", kanagawa_opts)
