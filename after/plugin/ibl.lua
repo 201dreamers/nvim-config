72d1709c920d9e1b5baef66ce0312c132a80b099
201dreamers <201dreamers@gmail.com>
2024-03-07 12:24:12 +0200

    setup indent lines

---
+++ after/plugin/ibl.lua
@@ -0,0 +1,54 @@
+local ok, ibl = pcall(require, "ibl")
+if not ok then
+	return
+end
+
+-- -----
+-- Setup
+-- -----
+local function setup_with_background_color()
+    local highlight = {
+        "CursorColumn",
+        "Whitespace",
+    }
+
+    ibl.setup({
+        indent = { highlight = highlight, char = "" },
+        whitespace = {
+            highlight = highlight,
+            remove_blankline_trail = false,
+        },
+        scope = { enabled = false },
+    })
+end
+
+
+local function setup_with_rainbow_lines()
+    local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+    }
+
+    local hooks = require "ibl.hooks"
+    -- create the highlight groups in the highlight setup hook, so they are reset
+    -- every time the colorscheme changes
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+        vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+        vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+        vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+        vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+        vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+        vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+    end)
+
+    ibl.setup { indent = { highlight = highlight } }
+end
+
+-- setup_with_rainbow_lines()
+setup_with_background_color()
